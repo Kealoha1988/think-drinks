1e5fb1b90e431104ac9559f85e90014973a874ed
Kealoha1988 <kealohaabe1988@gmail.com>
2021-05-06 14:08:18 -0400

    added components folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
 import './App.css';
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
     <h1>Hello World</h1>
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className="nav">
+      <h1>Hi I am the navbar</h1>
+    </div>
+  )
+}
